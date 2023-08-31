a2e4465ccf3c101e474c58ca4514b5683fe8d19e
Kyle Thomas <kthomas@hpe.com>
2023-08-31 14:02:28 +0100

    Displaying Data

--- internal/drivers/backtesting.py
+++ internal/drivers/backtesting.py
@@ -1,7 +1,6 @@
 # pylint: disable=missing-module-docstring
 from functools import reduce
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 Algorithms = ["mean_rev", "linear_model", "momentum"]
 
@@ -37,58 +36,98 @@ def strategy_implementation():
     Define a simple trading strategy, defines the activty to make and identifes changes
     """
     # ToDo This function only uses one Signal atm
-    generated_signals['Position'] = np.where(
-        generated_signals['Signals'] == 1, 'Buy',
-        np.where(generated_signals['Signals'] == -1, 'Sell', 'Hold'))
-    generated_signals['Position'] = generated_signals['Position'].fillna(0)
+    signal_to_position = {
+        1: 'Buy',
+        -1: 'Sell',
+        0: 'Hold'
+    }
+
+    generated_signals['Position'] = generated_signals['Signals'].map(signal_to_position)
+    generated_signals['Position'].fillna('Hold', inplace=True)
     generated_signals['Trade'] = generated_signals['Signals'].diff()
 
     for dataframe in alg_signals.values():
-        dataframe['Position'] = np.where(
-            dataframe['Signals'] == 1, 'Buy',
-            np.where(dataframe['Signals'] == -1, 'Sell', 'Hold'))
-        dataframe['Position'] = dataframe['Position'].fillna(0)
+        dataframe['Position'] = dataframe['Signals'].map(signal_to_position)
+        dataframe['Position'].fillna('Hold', inplace=True)
         dataframe['Trade'] = dataframe['Signals'].diff()
+
     return generated_signals
 
-def backtesting(signals_data):
-    """ Simple backtesting function """
-    initial_capital = 100000  # Initial capital in dollars
+def backtesting(signals_data, initial_capital=100000):
+    """
+    Backtesting function for evaluating trading strategy performance.
+    
+    Parameters:
+    - signals_data (pd.DataFrame): DataFrame containing trading signals and trade actions.
+    - initial_capital (float): Initial capital in dollars.
+    
+    Returns:
+    - pd.DataFrame: DataFrame with portfolio values after backtesting.
+    """
+    # Calculate portfolio values based on trading signals and actions
     signals_data['Portfolio'] = initial_capital + (signals_data['Trade'] * stock_data['Close'])
+
+    # Fill missing portfolio values using forward fill
     signals_data['Portfolio'] = signals_data['Portfolio'].fillna(method='ffill')
-    print(signals_data)
+
+    return signals_data
 
 def gen_metrics(signals_data):
-    """ Perfomance metrics """
+    """
+    Calculate performance metrics for a trading strategy.
+    
+    Parameters:
+    - signals_data (pd.DataFrame): DataFrame containing trading signals and portfolio values.
+    - risk_free_rate (float): Risk-free rate for calculating Sharpe ratio (default: 0.03).
+    
+    Returns:
+    - dict: A dictionary containing calculated performance metrics.
+    """
     returns = signals_data['Portfolio'].pct_change()
     cumulative_returns = (1 + returns).cumprod()
-    print(cumulative_returns)
     # Assuming 252 trading days in a year
     annualized_return = (cumulative_returns.iloc[-1]) ** (252 / len(signals_data)) - 1
     std_dev = returns.std()
     sharpe_ratio = (annualized_return - 0.03) / std_dev  # Assuming risk-free rate of 3%
     metrics = {
         'returns':returns,
-        'cummulative returns':cumulative_returns,
-        'annualised returns':annualized_return,
-        'standard deviation' : std_dev,
-        'share ratio':sharpe_ratio
+        'cummulative_returns':cumulative_returns,
+        'annualised_returns':annualized_return,
+        'std_dev' : std_dev,
+        'sharpe_ratio':sharpe_ratio
         }
     return metrics
 
-def show_data(metrics):
-    """ Step 6: Visualization """
+def show_data(title,metrics):
+    """
+    Visualize performance metrics of a trading algorithm.
+    
+    Parameters:
+    - metrics (dict): A dictionary containing performance metrics.
+    """
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
 
-    for key,value in metrics.items():
-        print(key, ':', value)
+    plt.title(title)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(stock_data.index, metrics['cummulative returns'], label='Strategy')
-    plt.xlabel('Date')
-    plt.ylabel('Cumulative Returns')
-    plt.title('Strategy Cumulative Returns')
-    plt.legend()
+    # Plot cumulative returns
+    axes[0].plot(metrics['cummulative_returns'])
+    axes[0].set_title('Cumulative Returns')
+
+    # Display metrics as a table
+    metrics_table = [
+        ['Annualized Return', metrics['annualised_returns']],
+        ['Standard Deviation', metrics['std_dev']],
+        ['Sharpe Ratio', metrics['sharpe_ratio']]
+    ]
+    axes[1].axis('off')
+    axes[1].table(cellText=metrics_table, loc='center')
+
+    # Adjust layout to prevent overlapping
+    plt.tight_layout()
+
+    # Display the plots
     plt.show()
+
 
 # Step 7: Parameter Optimization (Optional)
 # perform parameter optimization here by testing different parameter values.
@@ -110,7 +149,7 @@ if __name__=="__main__":
                    for Algorithms, signals in zip(Algorithms, signals)}
     print(strategy_implementation())
     print(alg_signals)
-    for model in alg_signals.values():
+    for name,model in zip(Algorithms, signals):
         backtesting(model)
         model_metrics = gen_metrics(model)
-        show_data(model_metrics)
+        show_data(name, model_metrics)
